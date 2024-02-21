68ba287e7f2f02751cb6654bd988eb880ffa63a4
ScottZhang812 <zscott812@foxmail.com>
2024-02-21 02:28:48 -0800

    p45 finished

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,18 @@ function App() {
     // </div>
     <div>
       <ExpandableText maxLength={10}>
-        dashu uhw euiqwh dhasuih weq dsaw w asd dwqd dwad wadawd wada wadawda
-        wada dawda w aw dada a dad adwda awdawd
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+        veritatis accusantium commodi inventore beatae ratione recusandae
+        expedita id. Explicabo labore error repudiandae! Voluptates, omnis eum
+        sequi eligendi accusantium amet tempora est saepe quas iure nostrum
+        reiciendis vel delectus labore. Et officiis fugit deleniti, esse
+        consequatur voluptates necessitatibus maxime dolore quam non labore
+        corporis veniam soluta a! Officia maxime praesentium, quo cum ducimus
+        expedita quidem eligendi, odio sit soluta consequuntur molestiae quod
+        quis repudiandae a, deserunt laborum porro unde dicta animi? Sit
+        necessitatibus nam repudiandae consequuntur magnam dignissimos est
+        ducimus ipsam velit sunt dolor, pariatur nemo tempore libero nulla a.
+        Explicabo!
       </ExpandableText>
     </div>
   );
